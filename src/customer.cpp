70c6916e8d8cd1a43591928a8c6a5757f3f8a4e1
Thadeu Knychala Tucci <thadeutucci@gmail.com>
2014-03-19 09:41:06 -0300

    add filter and mapper to customer

--- src/customer.cpp
+++ src/customer.cpp
@@ -1,5 +1,7 @@
 #include "customer.h"
 #include "ui_customer.h"
+#include <QSqlRecord>
+#include <QDebug>
 
 Customer::Customer(QWidget *parent) :
     QDialog(parent),
@@ -9,50 +11,98 @@ Customer::Customer(QWidget *parent) :
     this->setAttribute(Qt::WA_DeleteOnClose);
 
     CreateModel();
-
     CreateTable();
     MapFields();
+    CreateSignals();
+
 }
 
 void Customer::CreateModel()
 {
     model = new QSqlTableModel(this, QSqlDatabase::database());
+    model->setEditStrategy(QSqlTableModel::OnFieldChange);
     model->setTable("fornecedores");
     model->select();
+    model->setHeaderData(1, Qt::Horizontal, tr("Nome"));
+
+    proxyModel = new QSortFilterProxyModel;
+    proxyModel->setDynamicSortFilter(true);
+    proxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+    proxyModel->setSourceModel(model);
+    proxyModel->setFilterKeyColumn(1);
 }
 
 void Customer::CreateTable()
 {
-    QTableView *table = ui->tableView;
+    tableView = ui->tableView;
 
-    table->setModel(model);
-   // table->horizontalHeader()->moveSection(1, 0);
-   // table->sortByColumn(1, Qt::AscendingOrder);
-    for (int i = 0; i < 11; i++) {
-        if (i != 1)
-            table->hideColumn(i);
-    }
-    table->horizontalHeader()->setStretchLastSection(true);
-   // ui->tableView->selectRow(0);
+    tableView->setModel(proxyModel);
+    tableView->verticalHeader()->setVisible(false);
+    tableView->sortByColumn(1, Qt::AscendingOrder);
+    for (int i = 0; i < 11; i++)
+        tableView->hideColumn(i);
+    tableView->showColumn(1);
+    tableView->horizontalHeader()->setStretchLastSection(true);
+    tableView->selectRow(0);
 }
 
 void Customer::MapFields()
 {
     mapper = new QDataWidgetMapper(this);
+    mapper->setSubmitPolicy(QDataWidgetMapper::AutoSubmit);
     mapper->setModel(proxyModel);
-    mapper->addMapping(ui->nome, model->fieldIndex("nome"));
+    mapper->addMapping(ui->name, model->fieldIndex("nome"));
     mapper->addMapping(ui->cpf, model->fieldIndex("cpf"));
-    mapper->addMapping(ui->data, model->fieldIndex("datanascimento"));
-    mapper->addMapping(ui->endereco, model->fieldIndex("endereco"));
-    mapper->addMapping(ui->cidade, model->fieldIndex("cidade"));
-    mapper->addMapping(ui->celular, model->fieldIndex("celular"));
-    mapper->addMapping(ui->estado, model->fieldIndex("estado"));
-    mapper->addMapping(ui->fixo, model->fieldIndex("telfixo"));
-    mapper->addMapping(ui->data, model->fieldIndex("datanascimento"));
     mapper->addMapping(ui->rg   , model->fieldIndex("rg"));
+    mapper->addMapping(ui->birthday, model->fieldIndex("datanascimento"));
+    mapper->addMapping(ui->address, model->fieldIndex("endereco"));
+    mapper->addMapping(ui->city, model->fieldIndex("cidade"));
+    mapper->addMapping(ui->mobilePhoneNumber, model->fieldIndex("celular"));
+    mapper->addMapping(ui->state, model->fieldIndex("estado"));
+    mapper->addMapping(ui->phoneNumber, model->fieldIndex("telfixo"));
+
+    mapper->setCurrentIndex(ui->tableView->currentIndex().row());
 }
+
+void Customer::CreateSignals()
+{
+    connect(
+      tableView->selectionModel(),
+      SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)),
+      SLOT(selectedRow(const QItemSelection &, const QItemSelection &))
+     );
+}
+
+void Customer::selectedRow(const QItemSelection &selected, const QItemSelection &deselect)
+{
+    if (!selected.indexes().empty())
+        mapper->setCurrentModelIndex(selected.indexes().first());
+}
+
 
 Customer::~Customer()
 {
+    this->parentWidget()->close();
     delete ui;
+}
+
+void Customer::on_addCustomer_clicked()
+{
+    QSqlRecord rec( model->record() );
+    model->insertRecord(-1, rec );
+    model->select();
+    tableView->sortByColumn(1, Qt::AscendingOrder);
+    tableView->selectRow(0);
+}
+
+void Customer::on_delCustomer_clicked()
+{
+    int row = tableView->currentIndex().row();
+    proxyModel->removeRow(row);
+    model->select();
+}
+
+void Customer::on_filter_textChanged(const QString &str)
+{
+    proxyModel->setFilterRegExp(str);
 }
--- src/customer.h
+++ src/customer.h
@@ -4,6 +4,8 @@
 #include <QDialog>
 #include <QTableView>
 #include <QSqlTableModel>
+#include <QDataWidgetMapper>
+#include <QSortFilterProxyModel>
 
 namespace Ui {
 class Customer;
@@ -17,12 +19,22 @@ public:
     explicit Customer(QWidget *parent = 0);
     ~Customer();
 private:
-    void CreateModel();
     Ui::Customer *ui;
+    QTableView *tableView;
     QSqlTableModel *model;
     QDataWidgetMapper *mapper;
+    QSortFilterProxyModel *proxyModel;
+    void CreateModel();
     void CreateTable();
     void MapFields();
+    void CreateSignals();
+
+public slots:
+    void selectedRow(const QItemSelection &selected, const QItemSelection &deselect);
+private slots:
+    void on_addCustomer_clicked();
+    void on_delCustomer_clicked();
+    void on_filter_textChanged(const QString &str);
 };
 
 #endif // CUSTOMER_H
